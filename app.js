f107eb7d4e7326e6039ca088ae12afb7a7f649d3
HumoyunMirzo2003 <nabiyevxumoyunmirzo@gmail.com>
2022-11-30 22:18:06 +0500

    first commit

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-// uyga yil, kun, soat
+let body = document.querySelector("body");
 
-let sana = new Date(prompt("sanani kiriting"));
-let now = Date.now(); // 1970-01-01T00:00:00Z
-let yil;
-let kun ;
-let soat;
+setInterval(() => {
+    let a = Math.floor(Math.random() * 255);
+    let s = Math.floor(Math.random() * 255);
+    let d = Math.floor(Math.random() * 255);
+    
 
-yil = Math.floor((now - sana) / 1000 / 60 / 60 / 24 / 365.25) + ' yil';
-kun = Math.floor((now - sana) / 1000 / 60 / 60 / 24 % 365.25) + ' kun';
-soat = Math.floor((now - sana) / 1000 / 60 / 60 / 24 / 365.25 % 24) + ' soat';
-
-alert(`${yil} ${kun} ${soat}`);
-
-
-
-// 6 => 1 + 2 + 3 + 4 + 5 + 6 => 21
-
-// let a = +prompt('son kiriting');
-// var d = 0;
-// for(i = 0; i <= a; i++){
-//     d = i + d;
-// }
-// document.write(d);
+    body.style.backgroundColor = `rgb(${a}, ${s}, ${d})`;;
+  }, 3000);
 
 
 
 
-// 12 => 2, 4, 6, 8, 10, 12
 
-// let son = +prompt("son kiriting");
 
-// for (let i = 1; i <= son; i++) {
-//   if (i % 2 === 0) {
-//     console.log(`${i} juft`);
-//   }
-// }
+
+
+
+
+let chiroq = document.querySelector("#chiroq");
+
+chiroq.style.width = "500px";
+chiroq.style.height = "500px";
+chiroq.style.textAlign = "center";
+
+
+setInterval(() => {
+  let src = chiroq.getAttribute("src");
+  if (src === "./img/och.png") {
+    chiroq.setAttribute("src", "./img/yon.png");
+  } else {
+    chiroq.setAttribute("src", "./img/och.png");
+  }
+}, 2000);
